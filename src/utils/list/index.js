1b95da11208e581f926480d249266ba4312aaefd
toptaskalender <toptaskalender@gmail.com>
2022-03-07 14:04:50 +0300

    Delete comments

--- src/utils/list/index.js
+++ src/utils/list/index.js
@@ -34,37 +34,3 @@ export function getFilteredUnits(state) {
     return ageFilter && woodFilter && foodFilter && goldFilter;
   });
 }
-
-// export function getFilteredUnits(state) {
-//   return state.units.filter(u => {
-//     const { age: selectedAge } = state.currentState.filters;
-//     const {
-//       wood: woodRange,
-//       food: foodRange,
-//       gold: goldRange,
-//     } = state.currentState.filters.costs;
-
-//     let ageFilter = u.age === selectedAge;
-//     let woodFilter = true;
-//     let foodFilter = true;
-//     let goldFilter = true;
-
-//     if (selectedAge === 'All') {
-//       ageFilter = true;
-//     }
-
-//     if (u.cost && u.cost.Wood) {
-//       woodFilter = u.cost.Wood <= woodRange[1] && u.cost.Wood >= woodRange[0];
-//     }
-
-//     if (u.cost && u.cost.Food) {
-//       foodFilter = u.cost.Food <= foodRange[1] && u.cost.Food >= foodRange[0];
-//     }
-
-//     if (u.cost && u.cost.Gold) {
-//       goldFilter = u.cost.Gold <= goldRange[1] && u.cost.Gold >= goldRange[0];
-//     }
-
-//     return ageFilter && woodFilter && foodFilter && goldFilter;
-//   });
-// }
