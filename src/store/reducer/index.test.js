84d0efd040dab06275f03b321a99b0a29b67dbaf
toptaskalender <toptaskalender@gmail.com>
2022-03-08 12:22:27 +0300

    Update test for reducer

--- src/store/reducer/index.test.js
+++ src/store/reducer/index.test.js
@@ -17,7 +17,7 @@ describe('Reducer function', () => {
     },
   };
 
-  test('should immutably update pageTitle if action type is PAGE_TITLE_CHANGED', () => {
+  test('should immutably update pageTitle if action type is "PAGE_TITLE_CHANGED"', () => {
     const action = {
       type: 'PAGE_TITLE_CHANGED',
       payload: { pageTitle: 'Units' },
@@ -30,7 +30,7 @@ describe('Reducer function', () => {
     );
   });
 
-  test('should immutably update age if action type is AGE_CHANGED', () => {
+  test('should immutably update age if action type is "AGE_CHANGED"', () => {
     const action = {
       type: 'AGE_CHANGED',
       payload: { age: 'Imperial' },
@@ -41,7 +41,7 @@ describe('Reducer function', () => {
     expect(updatedState.currentState.filters.age).toEqual(action.payload.age);
   });
 
-  test('should immutably update cost range if action type is RANGE_CHANGED', () => {
+  test('should immutably update cost range if action type is "RANGE_CHANGED"', () => {
     const action = {
       type: 'RANGE_CHANGED',
       payload: { material: 'wood', range: [100, 150] },
@@ -54,7 +54,7 @@ describe('Reducer function', () => {
     ).toEqual(action.payload.range);
   });
 
-  test('should immutably reset cost range if action type is RANGE_RESET', () => {
+  test('should immutably reset cost range if action type is "RANGE_RESET"', () => {
     const action = {
       type: 'RANGE_RESET',
       payload: { material: 'food' },
